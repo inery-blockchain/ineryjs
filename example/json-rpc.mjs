f3ec067873ac6b16ff9f6a507f9047393b08469e
Briliant <ginanjar.lillunatic@gmail.com>
2022-12-03 13:47:00 +0700

    code comment

--- example/json-rpc.mjs
+++ example/json-rpc.mjs
@@ -6,18 +6,21 @@ dotenv.config()
 const url = process.env.NODE_URL
 const json_rpc = new JsonRpc(url)
 const private_key = process.env.PRIVATE_KEY; // private key
-console.log(private_key)
-const account = process.env.INERY_ACCOUNT
-const actor = process.env.INERY_ACCOUNT
-const signature  = new JsSignatureProvider([private_key]);
 
+const account = process.env.INERY_ACCOUNT // Inery Smart Contract Account to Call
+const actor = process.env.INERY_ACCOUNT // The Signer, should match with your provided Private Key
+const signature  = new JsSignatureProvider([private_key]); // creating Signer from private key
+
+// calling API
 const api = new Api({
     rpc: json_rpc,
     signatureProvider: signature
 })
 
+// A Function to create new data in our Valued Smart Contract, and call "create" function on our Smart contract
 async function create(id, user, data){
     try{
+        // create new transaction and sign it
         const tx = await api.transact({
             actions:[
                 {
@@ -35,11 +38,13 @@ async function create(id, user, data){
                 }
             ]
         },{broadcast:true,sign:true})
-        console.log(tx);
+
+        console.log(tx) // output the tx to terminal, it's Json Object
         console.log(tx.processed.action_traces[0].console)
     }catch(error){
         console.log(error)
     }
 }
 
+// call RPC that we created in create function
 create(5, account, "Create new Data via JSON RPC")
